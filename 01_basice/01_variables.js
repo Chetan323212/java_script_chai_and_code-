cf7409664b28f764cb8a4a13147fda326e349d92
Chetan323212 <95631630+Chetan323212@users.noreply.github.com>
2025-06-23 07:49:29 +0000

    01_variables.js

---
+++ 01_basice/01_variables.js
@@ -0,0 +1,22 @@
+const accountNumber = 12334  // The const variable is not change 
+let accountBalance = "99999"
+var accountEmail = "Email123@gmail.com"   // var is avoide to use becouse thay dont understand block scope  & functional scope   
+accountcity = "jamner"  // not do this 
+let accountstate;
+
+
+
+console.log(accountNumber);
+/*  
+{ accountEmail = "chatan323212@gmail.com" 
+accountBalance = "100000"
+accountcity = "Nagpur"     
+} This all are changeable  variables           
+  */
+
+
+accountEmail = "chatan323212@gmail.com" 
+accountBalance = "100000"
+accountcity = "Nagpur"                 
+
+console.table([accountEmail, accountBalance, accountcity, accountstate]) 
