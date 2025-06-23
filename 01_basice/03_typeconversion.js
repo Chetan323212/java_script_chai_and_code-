4f0e950431a0c95723fa5cdd78ea13a058aa54bd
Chetan323212 <95631630+Chetan323212@users.noreply.github.com>
2025-06-23 10:32:58 +0000

    03_typeconversion.js

---
+++ 01_basice/03_typeconversion.js
@@ -0,0 +1,12 @@
+// type conversion is mathod that convert the main type to other type of data 
+
+let score 
+
+//console.log(typeof score);
+let reviewscore = Boolean(score)
+console.log(reviewscore);
+
+
+
+
+
