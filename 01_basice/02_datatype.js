691e9064f621b553b0a4b98b762dec9b34916402
Chetan323212 <95631630+Chetan323212@users.noreply.github.com>
2025-06-23 07:47:54 +0000

    02_datatype

---
+++ 01_basice/02_datatype.js
@@ -0,0 +1,15 @@
+ "use strict"  // strict mode is on 
+
+  // alert("hello chetan")  => this throw error we use node.js not browser
+
+let a = 1123 // this is a integear datatype 
+let b = "Chetan" // this is a string datatype
+let c = true // this is a boolean datatype 
+let d  //  this is a Undefine datatype 
+let f = null // this is a null  that returns as a object datatype 
+
+console.table([typeof a,typeof b,typeof c,typeof d,typeof f]) // this line print a dayatype using typeof function 
+
+Object = e // a is a object datatype
+
+console.log(typeof Object);
